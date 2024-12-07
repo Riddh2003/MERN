9a38573a0889815bcfbc9934962cf15d48628d17
Riddh Modi <riddhmodi2003@gmail.com>
2024-12-07 21:51:17 +0530

    add css and components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import "../assets/css/Header.css"
 
 function Header() {
     return (
-        <div>
+        <div className='header'>
             <h1>HEADER COMPONENT</h1>
         </div>
     )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import "../assets/css/Home.css"
 
 export const Home = () => {
   return (
-    <div>
+    <div className='home'>
         <h1>HOME COMPONENT</h1>
     </div>
   )
